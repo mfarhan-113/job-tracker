d5bcab6f56c482af4a5173a004d1205f98c9bb8c
User <user@example.com>
2025-10-28 13:09:53 +0500

    khud kiya

--- apptrack-backend/config/urls.py
+++ apptrack-backend/config/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import JsonResponse
 from django.urls import include, path, re_path
+from rest_framework.routers import DefaultRouter
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
